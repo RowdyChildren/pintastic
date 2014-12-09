245f4e9c5bb1c5e13f5e224befe2217394bcd385
Riley Childs <riley@tfsgeo.com>
2014-12-08 19:27:06 -0500

    added stuff needed to create framework

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+	has_many :boards
+	has_many :pins
+
+
+	# validates :first_name, 
+	# presence: true,
+	# length { minimum: 2, maximum: 20 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :boards
+
+  resources :pins
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
