bf31fc767564636e25a7d24167c9ce1bc96f2cea
Riley Childs <riley@tfsgeo.com>
2014-12-08 20:59:42 -0500

    finishing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+ 
+ get 'home' => 'users#index'
+
+ delete '/pins' => "pins#delete_all"
   resources :boards
 
   resources :pins
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
